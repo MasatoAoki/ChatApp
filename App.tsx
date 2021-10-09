9d39e43795731d9ba986617c01fed1107dd67492
masato <aoki.masato.lmasato@gmail.com>
2021-10-09 14:04:06 +0900

    M_01

--- App.tsx
+++ App.tsx
@@ -1,24 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ChatScreen } from './src/screens/ChatScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return <ChatScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 //main
 //develop
